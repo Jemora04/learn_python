e41a9a4e5fb69d0c2d9ccde54696c59ef061d222
jemora04 <jm4588080@gmail.com>
2024-03-21 17:44:33 -0500

    New files

--- cursos/02-strings.py
+++ cursos/02-strings.py
@@ -2,7 +2,7 @@ texto = "Hola Mundo"
 print(texto.upper())
 print(texto.lower())
 print(texto.find("M"))
-nuevoTexto = texto.replace("Mun", "chanchito feliz")
+nuevoTexto = texto.replace("Mun", "chanchitos feliz")
 print(nuevoTexto, nuevoTexto)
 print("Mundo" in texto)
 
